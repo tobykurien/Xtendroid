51da66a2b55c9e89d88abdd945874b1d8a9f2830
tobykurien@gmail.com <tobykurien@gmail.com>
2014-05-05 18:45:51 +0200

    added example of loading large number of items in a list using LazyList

--- XtendroidTest/src/org/xtendroid/xtendroidtest/MainActivity.xtend
+++ XtendroidTest/src/org/xtendroid/xtendroidtest/MainActivity.xtend
@@ -2,10 +2,17 @@ package org.xtendroid.xtendroidtest
 
 import org.xtendroid.app.AndroidActivity
 import org.xtendroid.app.OnCreate
+import org.xtendroid.adapter.BeanAdapter
+
+import static extension org.xtendroid.xtendroidtest.db.DbService.*
+import org.xtendroid.xtendroidtest.models.ManyItem
 
 @AndroidActivity(layout=R.layout.activity_main) class MainActivity {
 	@OnCreate
 	def create() {
 		mainHello.text = getString(R.string.hello_world)
+		
+		var manyItems = db.lazyFindAll("manyitems", "id", ManyItem)
+		mainList.adapter = new BeanAdapter(this, R.layout.main_list_row, manyItems)
 	}
 }
--- XtendroidTest/xtend-gen/org/xtendroid/xtendroidtest/MainActivity.java
+++ XtendroidTest/xtend-gen/org/xtendroid/xtendroidtest/MainActivity.java
@@ -2,11 +2,16 @@ package org.xtendroid.xtendroidtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
+import org.xtendroid.adapter.BeanAdapter;
 import org.xtendroid.app.AndroidActivity;
 import org.xtendroid.app.OnCreate;
+import org.xtendroid.db.LazyList;
 import org.xtendroid.xtendroidtest.MainActivity_CallBacks;
 import org.xtendroid.xtendroidtest.R;
+import org.xtendroid.xtendroidtest.db.DbService;
+import org.xtendroid.xtendroidtest.models.ManyItem;
 
 @AndroidActivity(layout = R.layout.activity_main)
 @SuppressWarnings("all")
@@ -16,6 +21,11 @@ public class MainActivity extends Activity implements MainActivity_CallBacks {
     TextView _mainHello = this.getMainHello();
     String _string = this.getString(R.string.hello_world);
     _mainHello.setText(_string);
+    DbService _db = DbService.getDb(this);
+    LazyList<ManyItem> manyItems = _db.<ManyItem>lazyFindAll("manyitems", "id", ManyItem.class);
+    ListView _mainList = this.getMainList();
+    BeanAdapter<ManyItem> _beanAdapter = new BeanAdapter<ManyItem>(this, R.layout.main_list_row, manyItems);
+    _mainList.setAdapter(_beanAdapter);
   }
   
   public void onCreate(final Bundle savedInstanceState) {
@@ -26,5 +36,9 @@ public class MainActivity extends Activity implements MainActivity_CallBacks {
   
   public TextView getMainHello() {
     return (TextView) findViewById(R.id.main_hello);
+  }
+  
+  public ListView getMainList() {
+    return (ListView) findViewById(R.id.main_list);
   }
 }
