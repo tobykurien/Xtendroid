f593e9b2670db3d900a9bc49b965f6d00d9344a5
Toby Kurien <toby@tobykurien.com>
2016-11-29 12:24:00 +0200

    remove warnings and only run test if compile succeeds

--- run_specific_test.sh
+++ run_specific_test.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-./gradlew XtendroidTest:installDebugAndroidTest
-adb shell am instrument -w -e debug false -e class org.xtendroid.xtendroidtest.test.$1 org.xtendroid.xtendroidtest.test/android.support.test.runner.AndroidJUnitRunner
+./gradlew -q XtendroidTest:installDebugAndroidTest && adb shell am instrument -w -e debug false -e class org.xtendroid.xtendroidtest.test.$1 org.xtendroid.xtendroidtest.test/android.support.test.runner.AndroidJUnitRunner
 
