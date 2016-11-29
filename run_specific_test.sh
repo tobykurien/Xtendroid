154ad82533cb8618a329a3d8f2afd9c54c6e4d84
Toby Kurien <toby@tobykurien.com>
2016-11-29 12:59:16 +0200

    build and run new xtendroid code
    unfortunately this still seems to require running twice to pick up changes to active annotations

--- run_specific_test.sh
+++ run_specific_test.sh
@@ -3,4 +3,5 @@
 # Compile and run a specific test from XtendroidTestCases 
 # e.g. ./run_specific_test.sh JsonTest
 
-./gradlew -q XtendroidTest:installDebugAndroidTest && adb shell am instrument -w -e debug false -e class org.xtendroid.xtendroidtest.test.$1 org.xtendroid.xtendroidtest.test/android.support.test.runner.AndroidJUnitRunner
+./gradlew -q Xtendroid:assembleDebug XtendroidTest:assembleDebug XtendroidTest:installDebugAndroidTest && 
+   adb shell am instrument -w -e debug false -e class org.xtendroid.xtendroidtest.test.$1 org.xtendroid.xtendroidtest.test/android.support.test.runner.AndroidJUnitRunner
