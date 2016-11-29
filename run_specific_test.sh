dbc351bfe8d8a395346d0c9900cc88a237bafb28
Toby Kurien <toby@tobykurien.com>
2016-11-29 13:27:29 +0200

    trying to make building repeatable

--- run_specific_test.sh
+++ run_specific_test.sh
@@ -3,5 +3,6 @@
 # Compile and run a specific test from XtendroidTestCases 
 # e.g. ./run_specific_test.sh JsonTest
 
-./gradlew -q XtendroidTest:assembleDebug XtendroidTest:installDebugAndroidTest && 
+./gradlew -q XtendroidTest:assembleDebug &&
+   ./gradlew -q XtendroidTest:installDebugAndroidTest && 
    adb shell am instrument -w -e debug false -e class org.xtendroid.xtendroidtest.test.$1 org.xtendroid.xtendroidtest.test/android.support.test.runner.AndroidJUnitRunner
